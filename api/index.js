5e5e64d8833d2a601255c410aa65060fe73e5b3f
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-04 12:00:50 +0530

    token verify

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -13,6 +14,7 @@ mongoose
   .catch((err) => console.log(err));
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 app.listen(8800, () => {
   console.log("backend server is running");
 });
