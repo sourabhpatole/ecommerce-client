5c87dfc474ca4fe930ba48bb063be882e1516725
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-05 20:18:38 +0530

    login page with auth context

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import { login } from "../../context/authContext/apiCalls";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import "./login.css";
+export default function Login() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { isFetching, dispatch } = useContext(AuthContext);
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    login({ email, password }, dispatch);
+  };
+  return (
+    <div className="login">
+      <form className="loginForm">
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="loginButton"
+          onClick={handleLogin}
+          disabled={isFetching}
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
